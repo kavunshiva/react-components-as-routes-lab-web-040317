4a410099808c050708d97278e937de412a09f216
kavunshiva <kavunshiva@gmail.com>
2017-06-12 14:27:57 -0400

    break it fast, make it work

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {
+        movies.map(movie => {
+          return (
+            <div>
+              <h3>{`${movie.title}`}</h3>
+              {`${movie.time} minutes`}
+              {movie.genres.map(genre => <ul>{genre}</ul>)}
+            </div>
+          )
+        })
+      }
     </div>
-  );
+  )
 };
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,17 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className="navbar navbar-inverse">
+      <div className='container-fluid'>
+        <div className='navbar-header'>
+          <NavLink to='/'>Home</NavLink>
+        </div>
+        <ul className="nav navbar-nav">
+          <li><NavLink to="/movies">Movies</NavLink></li>
+          <li><NavLink to="/directors">Directors</NavLink></li>
+          <li><NavLink to="/actors">Actors</NavLink></li>
+        </ul>
+      </div>
     </div>
   );
 };
